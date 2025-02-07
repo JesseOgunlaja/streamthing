0489244da0c342b1aab42090a541274a7cdf662e
JesseOgunlaja <jesseogunlaja@gmail.com>
2025-02-07 21:07:01 +0000

    fix: profile picture creation

--- actions/lib/lib.ts
+++ actions/lib/lib.ts
@@ -5,18 +5,18 @@ import sharp from "sharp";
 export async function createProfilePicture(letter: string, id: string) {
   const imageBuffer = await sharp({
     create: {
-      width: 100,
-      height: 100,
+      width: 400,
+      height: 400,
       channels: 4,
-      background: { r: 255, g: 255, b: 255, alpha: 0 },
+      background: { r: 0, g: 0, b: 0, alpha: 0 },
     },
   })
     .composite([
       {
         input: Buffer.from(
-          `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="50" cy="50" r="50" fill="${getRandomColor()}" />
-            <text x="50" y="50" font-family="Arial" font-size="45" fill="white" text-anchor="middle" dy=".35em">
+          `<svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="200" cy="200" r="200" fill="${getRandomColor()}" />
+            <text x="200" y="200" font-family="Arial" font-size="200" font-weight="bold" fill="white" text-anchor="middle" dy=".35em">
               ${letter.toUpperCase()}
             </text>
           </svg>`
