280f5c624c4b442edf40ad24a7585d8dd1e678c1
JesseOgunlaja <jesseogunlaja@gmail.com>
2025-02-10 11:12:43 +0000

    fix: profile picture creation

--- actions/lib/lib.ts
+++ actions/lib/lib.ts
@@ -1,39 +1,21 @@
 import { utapi } from "@/app/api/uploadthing/core";
 import { getContrast } from "polished";
-import sharp from "sharp";
 
 export async function createProfilePicture(letter: string, id: string) {
-  const imageBuffer = await sharp({
-    create: {
-      width: 400,
-      height: 400,
-      channels: 4,
-      background: { r: 0, g: 0, b: 0, alpha: 0 },
-    },
-  })
-    .composite([
-      {
-        input: Buffer.from(
-          `<svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="200" cy="200" r="200" fill="${getRandomColor()}" />
-            <text x="200" y="200" font-family="Arial" font-size="200" font-weight="bold" fill="white" text-anchor="middle" dy=".35em">
-              ${letter.toUpperCase()}
-            </text>
-          </svg>`
-        ),
-        top: 0,
-        left: 0,
-      },
-    ])
-    .png()
-    .toBuffer();
+  const size = 200;
+  const svgContent = `<svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="${size / 2}" cy="${size / 2}" r="${size / 2}" fill="${getRandomColor()}" />
+    <text x="${size / 2}" y="${size / 2}" font-family="Arial" font-size="${size / 2}" font-weight="bold" fill="white" text-anchor="middle" dy=".35em">
+      ${letter.toUpperCase()}
+    </text>
+  </svg>`;
 
-  const fileName = `profile-picture-${id}.png`;
-  const fileOptions = { type: "image/png" };
-  const file = new File([imageBuffer], fileName, fileOptions);
+  const svgBuffer = Buffer.from(svgContent);
+  const fileName = `profile-picture-${id}.svg`;
+  const fileOptions = { type: "image/svg+xml" };
+  const file = new File([svgBuffer], fileName, fileOptions);
 
   const { data } = await utapi.uploadFiles(file);
-
   return { imageURL: data!.url, imageID: data!.key };
 }
 
