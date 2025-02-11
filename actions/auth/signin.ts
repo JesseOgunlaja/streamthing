8f7eb4e755465daf69d38ee7a3d1d13342a70b71
JesseOgunlaja <jesseogunlaja@gmail.com>
2025-02-11 11:23:37 +0000

    fix: ratelimiting and error handling

--- actions/auth/signin.ts
+++ actions/auth/signin.ts
@@ -1,48 +1,64 @@
 "use server";
 
 import { createSessionId, setSessionCookie, signJWT } from "@/lib/auth";
-import { rateLimitByIP } from "@/lib/ratelimit";
+import { rateLimitByIP, RateLimitError } from "@/lib/ratelimit";
 import { getUserByEmail } from "@/lib/redis";
 import { compare as comparePasswords } from "bcryptjs";
 
 export async function signin_internal(email: string, password: string) {
-  rateLimitByIP({
-    name: "sign-in",
-    limit: 5,
-    duration: 10,
-    errorResponse: {
-      ok: false,
-      message: "Too many requests, try again later",
-    },
-    limitOnErrorsOnly: true,
-  });
+  try {
+    const ratelimit = await rateLimitByIP({
+      name: "sign-in",
+      limit: 5,
+      duration: 10,
+      errorResponse: {
+        ok: false,
+        message: "Too many requests, try again later",
+      },
+      limitOnErrorsOnly: true,
+    });
 
-  const user = await getUserByEmail(email);
-  if (!user || !user.auth.includes("Internal")) {
+    const user = await getUserByEmail(email);
+    if (!user || !user.auth.includes("Internal")) {
+      ratelimit.incrementRatelimit();
+      return {
+        ok: false,
+        message: "Invalid credentials",
+      };
+    }
+
+    const isPasswordCorrect = await comparePasswords(password, user.password);
+    if (!isPasswordCorrect) {
+      ratelimit.incrementRatelimit();
+      return {
+        ok: false,
+        message: "Invalid credentials",
+      };
+    }
+
+    const sessionID = await createSessionId(email, user.id, "Internal");
+    const jwt = await signJWT({
+      sessionID,
+      type: "session",
+    });
+
+    await setSessionCookie(jwt, sessionID);
+
+    return {
+      ok: true,
+      message: "Valid credentials",
+    } as const;
+  } catch (err) {
+    if (err instanceof RateLimitError) {
+      return {
+        ok: false,
+        message: "Ratelimited, please try again later",
+      };
+    }
+
     return {
       ok: false,
       message: "Invalid credentials",
     };
   }
-
-  const isPasswordCorrect = await comparePasswords(password, user.password);
-  if (!isPasswordCorrect) {
-    return {
-      ok: false,
-      message: "Invalid credentials",
-    };
-  }
-
-  const sessionID = await createSessionId(email, user.id, "Internal");
-  const jwt = await signJWT({
-    sessionID,
-    type: "session",
-  });
-
-  await setSessionCookie(jwt, sessionID);
-
-  return {
-    ok: true,
-    message: "Valid credentials",
-  } as const;
 }
