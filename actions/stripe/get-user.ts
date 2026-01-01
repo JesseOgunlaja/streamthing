2e3ba36f3a9a5b51d5f28dae89a7ad6ada3efbe3
JesseOgunlaja <jesseogunlaja@gmail.com>
2026-01-01 16:00:33 +0000

    fix(stripe): make changes needed for Stripe v20

--- actions/stripe/get-user.ts
+++ actions/stripe/get-user.ts
@@ -5,51 +5,53 @@ import { formatTimestamp } from "@/lib/lib";
 import { stripeInstance, StripeTypes } from "@/lib/stripe";
 
 export async function getStripeUserData(
-  customerID: string,
-  subscriptionID: string
+    customerID: string,
+    subscriptionID: string
 ) {
-  try {
-    const subscription = await stripeInstance.subscriptions.retrieve(
-      subscriptionID,
-      {
-        expand: ["default_payment_method"],
-      }
-    );
+    try {
+        const subscription = await stripeInstance.subscriptions.retrieve(
+            subscriptionID,
+            {
+                expand: ["default_payment_method"],
+            }
+        );
 
-    const paymentMethods = await stripeInstance.paymentMethods.list({
-      customer: customerID,
-    });
+        const paymentMethods = await stripeInstance.paymentMethods.list({
+            customer: customerID,
+        });
 
-    const activeMethod =
-      subscription.default_payment_method as StripeTypes.PaymentMethod;
+        const activeMethod =
+            subscription.default_payment_method as StripeTypes.PaymentMethod;
 
-    const methods = paymentMethods.data.map((method) => {
-      return {
-        active: activeMethod.id === method.id,
-        type: "card",
-        id: method!.id,
-        brand: method.card!.brand,
-        exp: `${monthNames[method.card!.exp_month - 1]}, ${
-          method.card!.exp_year
-        }`,
-        number: `•••• •••• •••• ${method.card!.last4}`,
-      } as const;
-    });
+        const methods = paymentMethods.data.map((method) => {
+            return {
+                active: activeMethod.id === method.id,
+                type: "card",
+                id: method!.id,
+                brand: method.card!.brand,
+                exp: `${monthNames[method.card!.exp_month - 1]}, ${
+                    method.card!.exp_year
+                }`,
+                number: `•••• •••• •••• ${method.card!.last4}`,
+            } as const;
+        });
 
-    const renewing = formatTimestamp(subscription.current_period_end);
+        const renewing = formatTimestamp(
+            subscription.items.data[0].current_period_end
+        );
 
-    return {
-      methods,
-      subscription: {
-        renewing,
-      },
-    };
-  } catch {
-    return {
-      methods: [],
-      subscription: {
-        renewing: "N/A",
-      },
-    };
-  }
+        return {
+            methods,
+            subscription: {
+                renewing,
+            },
+        };
+    } catch {
+        return {
+            methods: [],
+            subscription: {
+                renewing: "N/A",
+            },
+        };
+    }
 }
--- actions/stripe/update-plan.ts
+++ actions/stripe/update-plan.ts
@@ -1,90 +1,100 @@
 "use server";
 
-import { cookies } from "next/headers";
 import { BillingPlan, stripePricingTokens } from "@/constants/billing";
 import { serversByRegion } from "@/constants/constants";
-import { VM_FETCH_CONFIG } from "@/lib/server-utils";
 import { isSignedIn } from "@/lib/auth";
 import { formatTimestamp } from "@/lib/lib";
 import { kv } from "@/lib/redis";
+import { VM_FETCH_CONFIG } from "@/lib/server-utils";
 import { StripeTypes, stripeInstance } from "@/lib/stripe";
+import { cookies } from "next/headers";
 
 async function getSubscription(customerID: string) {
-	const subscriptions = await stripeInstance.subscriptions.list({
-		customer: customerID,
-	});
-	const subscription = subscriptions.data[0];
+    const subscriptions = await stripeInstance.subscriptions.list({
+        customer: customerID,
+    });
+    const subscription = subscriptions.data[0];
 
-	return subscription as StripeTypes.Subscription & { plan: StripeTypes.Plan };
+    return subscription as StripeTypes.Subscription & {
+        plan: StripeTypes.Plan;
+    };
 }
 
 export async function getSubscriptionRenewDate() {
-	try {
-		const access_token = (await cookies()).get("access_token")?.value || "";
-		const { user, signedIn } = await isSignedIn(access_token);
-		if (!signedIn) return "Unauthorized";
+    try {
+        const access_token = (await cookies()).get("access_token")?.value || "";
+        const { user, signedIn } = await isSignedIn(access_token);
+        if (!signedIn) return "Unauthorized";
 
-		if (user.plan === "Hobby") return "No subscription";
+        if (user.plan === "Hobby") return "No subscription";
 
-		const subscription = await getSubscription(user.stripe_customer_id);
-		const renewing = formatTimestamp(subscription.current_period_end);
+        const subscription = await getSubscription(user.stripe_customer_id);
+        const renewing = formatTimestamp(
+            subscription.items.data[0].current_period_end
+        );
 
-		return renewing as `${number}/${number}`;
-	} catch (err) {
-		return "An unexpected error occurred, please try again";
-	}
+        return renewing as `${number}/${number}`;
+    } catch (err) {
+        return "An unexpected error occurred, please try again";
+    }
 }
 
 export async function updateSubscriptionPlan(newPlan: BillingPlan) {
-	try {
-		const access_token = (await cookies()).get("access_token")?.value || "";
-		const { user, signedIn } = await isSignedIn(access_token);
-		if (!signedIn) return "Unauthorized";
+    try {
+        const access_token = (await cookies()).get("access_token")?.value || "";
+        const { user, signedIn } = await isSignedIn(access_token);
+        if (!signedIn) return "Unauthorized";
 
-		const subscription = await getSubscription(user.stripe_customer_id);
-		const newPlanID = stripePricingTokens[newPlan];
+        const subscription = await getSubscription(user.stripe_customer_id);
+        const newPlanID = stripePricingTokens[newPlan];
 
-		await Promise.all([
-			kv.main.json.set(`user-${user.email}`, "$.plan", JSON.stringify(newPlan)),
-			stripeInstance.subscriptions.update(subscription.id, {
-				cancel_at_period_end: false,
-				proration_behavior: "create_prorations",
-				items: [
-					{
-						id: subscription.items.data[0].id,
-						plan: newPlanID,
-					},
-				],
-			}),
-		]);
+        await Promise.all([
+            kv.main.json.set(
+                `user-${user.email}`,
+                "$.plan",
+                JSON.stringify(newPlan)
+            ),
+            stripeInstance.subscriptions.update(subscription.id, {
+                cancel_at_period_end: false,
+                proration_behavior: "create_prorations",
+                items: [
+                    {
+                        id: subscription.items.data[0].id,
+                        plan: newPlanID,
+                    },
+                ],
+            }),
+        ]);
 
-		const uniqueRegions = new Set(user.servers.map((server) => server.region));
-		Array.from(uniqueRegions).forEach((region) => {
-			fetch(
-				`${serversByRegion[region]}/reset-user-cache/${user.id}`,
-				VM_FETCH_CONFIG,
-			);
-		});
+        const uniqueRegions = new Set(
+            user.servers.map((server) => server.region)
+        );
+        Array.from(uniqueRegions).forEach((region) => {
+            fetch(
+                `${serversByRegion[region]}/reset-user-cache/${user.id}`,
+                VM_FETCH_CONFIG
+            );
+        });
 
-		return "Success";
-	} catch {
-		return "An unexpected error occurred, please try again";
-	}
+        return "Success";
+    } catch {
+        return "An unexpected error occurred, please try again";
+    }
 }
 
 export async function cancelSubscription() {
-	try {
-		const access_token = (await cookies()).get("access_token")?.value || "";
-		const { user, signedIn } = await isSignedIn(access_token);
-		if (!signedIn) return "Unauthorized";
+    try {
+        const access_token = (await cookies()).get("access_token")?.value || "";
+        const { user, signedIn } = await isSignedIn(access_token);
+        if (!signedIn) return "Unauthorized";
 
-		const subscription = await getSubscription(user.stripe_customer_id);
-		await stripeInstance.subscriptions.update(subscription.id, {
-			cancel_at_period_end: true,
-		});
+        const subscription = await getSubscription(user.stripe_customer_id);
+        await stripeInstance.subscriptions.update(subscription.id, {
+            cancel_at_period_end: true,
+        });
 
-		return "Success";
-	} catch {
-		return "An unexpected error occurred, please try again";
-	}
+        return "Success";
+    } catch {
+        return "An unexpected error occurred, please try again";
+    }
 }
